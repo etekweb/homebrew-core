0b2655bf268d5afdb137b0c2e801de20eaf88e63
Ethan <ethan@etekweb.net>
2020-12-03 13:44:53 -0600

    Fix broken homepage link for mboxgrep formula

--- Formula/mboxgrep.rb
+++ Formula/mboxgrep.rb
@@ -1,6 +1,6 @@
 class Mboxgrep < Formula
   desc "Scan a mailbox for messages matching a regular expression"
-  homepage "https://datatipp.se/mboxgrep/"
+  homepage "https://mboxgrep.datatipp.se/"
   url "https://downloads.sourceforge.net/project/mboxgrep/mboxgrep/0.7.9/mboxgrep-0.7.9.tar.gz"
   sha256 "78d375a05c3520fad4bca88509d4da0dbe9fba31f36790bd20880e212acd99d7"
   license "GPL-2.0"
